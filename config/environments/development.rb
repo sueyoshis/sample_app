00f910b07d620232bd58a0162e2dc3f6482baca4
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-03-03 03:00:31 +0000

    [Add]ホスト許可の追記

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "5b49a5ace93f4c8db5e0b7f6cfb146e1.vfs.cloud9.us-east-1.amazonaws.com"
 end
